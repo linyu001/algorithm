767302235158b589da9dfdcfb88a50dd5f7242d3
2 <1923477173@qq.com>
2019-12-09 14:43:40 +0800

    求两个数的最大公约数

---
+++ src/actual/MaxDivisor.java
@@ -0,0 +1,28 @@
+package actual;
+
+/**
+ * 求两个数的最大公约数
+ * 更相减损术和位移运算
+ */
+public class MaxDivisor {
+    public static int gcd(int a,int b){
+        if(a==b){
+            return a;
+        }
+        if((a&1)==0&&(b&1)==0){
+            return gcd(a>>1,b>>1)<<1;
+        }else if ((a&1)==0&&(b&1)==1){
+            return gcd(a>>1,b);
+        }else if ((a&1)==1&&(b&1)==0){
+            return gcd(a,b>>1);
+        }else {
+            int big=a>b?a:b;
+            int small=a<b?a:b;
+            return gcd(big-small,small);
+        }
+
+    }
+    public static void main(String[] args) {
+        System.out.println(gcd(20,15));
+    }
+}
